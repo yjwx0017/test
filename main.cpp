a98268ac1a0346ef4bd3816f2b0b60c8d37807e9
testUserName0017 <761225349@qq.com>
2016-09-09 16:46:54 +0800

    Add test for vector

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void testVector();
 
 int main()
 {
 	cout << "Hello, world!" << endl;
 	return 0;
+}
+
+void testVector()
+{
+	std::vector<int> numbers;
+	numbers.push_back(1);
+	numbers.push_back(2);
 }

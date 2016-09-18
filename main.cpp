931ed8b0954da9e5789c8f2eadcace73766ae50e
ZhouYuanchao <761225349@qq.com>
2016-09-19 00:40:47 +0800

    Test boost::lexical_cast

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <boost/lexical_cast.hpp>
+#include <sstream>
 using namespace std;
 
 void testVector();
+void testLexicalCast();
 
 int main()
 {
 	cout << "Hello, world!" << endl;
+
+	testLexicalCast();
 	return 0;
 }
 
@@ -17,4 +22,27 @@ void testVector()
 	numbers.push_back(2);
 	for (int i = 0; i < (int)numbers.size(); ++i)
 		cout << i << endl;
+}
+
+void testLexicalCast()
+{
+	// 以下会抛出异常
+	//cout << boost::lexical_cast<int>("  123  ") << endl;
+	//cout << boost::lexical_cast<int>("123  ") << endl;
+	//cout << boost::lexical_cast<int>("12 3") << endl;
+	//cout << boost::lexical_cast<int>("1 2 3") << endl;
+
+	// 空字符串会抛出异常
+	//cout << boost::lexical_cast<int>("") << endl;
+
+	// 不支持十六进制
+	//cout << boost::lexical_cast<int>("0x0362") << endl;
+
+	// 十六进制字符串转成整数 0x前缀可有可无
+	// C++11 中的 std::stoi <string>
+	stringstream ss;
+	ss << hex << "0x0362f"; // 13871  
+	int i;
+	ss >> i;
+	cout << i << endl;
 }

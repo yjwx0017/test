fb00ee54f1959ba1496e0643ad04644b5311f66a
yjwx0017 <761225349@qq.com>
2016-10-21 23:36:07 +0800

    Test PIL, ImageDraw module

--- python-codes/testPIL.py
+++ python-codes/testPIL.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
-import Image
+import Image, ImageDraw, ImageFont
 
 im = Image.open("image.jpg")
 print(im.format, im.size, im.mode)
 
 box = (0, 0, 400, 400)
 region = im.crop(box)
+# region.show()
 
-raw_input('')
+draw = ImageDraw.Draw(im)
+draw.line((0, 0, 200, 200), fill = 128)
+draw.arc((0, 0, 200, 200), 0, 45, fill = (0, 255, 0))
+draw.text((0, 0), "Hello PIL", fill=255)
+
+font = ImageFont.truetype("arial.ttf", 20)
+draw.text((20, 20), "Hello Boy!", font=font, fill=(0, 255, 0))
+
+del draw
+
+im.show()
+
 

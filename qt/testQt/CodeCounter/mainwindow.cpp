f52942b8de9eec7d1f5da6e4083ab735e1ed1017
yjwx0017 <761225349@qq.com>
2017-02-21 23:55:54 +0800

    Update testQt/CodeCounter

--- qt/testQt/CodeCounter/mainwindow.cpp
+++ qt/testQt/CodeCounter/mainwindow.cpp
@@ -1,8 +1,24 @@
 #include "mainwindow.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
-	: QMainWindow(parent)
+	: QWidget(parent)
 {
+	QLineEdit* edit = new QLineEdit;
+	edit->setReadOnly(true);
+
+	QPushButton* button = new QPushButton(tr("浏览"));
+
+	QHBoxLayout* folderLayout = new QHBoxLayout;
+	folderLayout->addWidget(edit);
+	folderLayout->addWidget(button);
+
+
+	QVBoxLayout* verticalLayout = new QVBoxLayout(this);
+	verticalLayout->addLayout(folderLayout);
 }
 
 MainWindow::~MainWindow()
